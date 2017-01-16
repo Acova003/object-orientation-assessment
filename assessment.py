e4a2578d675eaabe847282551cbb184b489f9bca
Amee <ameecova@yahoo.com>
2017-01-15 20:58:00 -0800

    final OO assessment

---
+++ assessment.py
@@ -0,0 +1,59 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+   -Smarter dictionaries: keeps data closer together and keeps track of the functions
+   that relate to a data structure. Keeps cleaner and more efficient code. 
+   -Flexible structure: enables a program to reuse the same functions with variation
+   in a more efficient way. 
+   -Hierarchies: object orientation can provide a hierchy of instances that are inherited
+   from one class to another. 
+
+2. What is a class?
+Classes are a core part of object-oriented programming. A class is like a mini 
+program within a program that holds it's own set of instructions. 
+A blueprint for the program. 
+
+3. What is an instance attribute?
+The process of instantiation. It can ask in an instance what class it is. 
+
+4. What is a method?
+A method is like a function defined on a class. 
+
+5. What is an instance in object orientation?
+An individual occurance of a class. 
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+   A class is a type "type of thing." It is a bigger puzzle piece in code. An 
+   instance is an individual occurance. It is one circumstance in a code. 
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
+
+class Student(student_info)
+
+    def __init__(self, first, last, address):
+        self.first = first
+        self.last = last
+        self.address = address
+
+    def student_profile(self)
+        {'first: self
+        last: last
+        address: address
+        '}
+    def new_question(self, question):
+        self.question = question
+
+        {'question': 'What is the capital of Alberta?',
+ 'correct_answer': self}
+
+
+
+
